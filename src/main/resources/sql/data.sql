bf7d9316a70b64db80a9f3c4c8308b9e3246006c
dazhi <dazhi_6@163.com>
2020-05-25 23:05:21 +0800

    添加表

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -9,14 +9,10 @@ VALUES
 ('大智伟业', '销售业', '大智', '13765484561'),
 ('大智制造', '制造业', '大智', '13798465123');
 
-
-
 INSERT INTO shop_category (company_id, title, parentid) 
 VALUES 
 (1, '蔬菜', 0),
 (1, '水果', 0);
-
-
 
 INSERT INTO shop_product (company_id, category_id, title, description) 
 VALUES 
